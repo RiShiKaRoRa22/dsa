89490151aaf0040dd07a9a717acf0505725d8621
Rishika <148088151+RiShiKaRoRa22@users.noreply.github.com>
2025-10-17 18:30:59 +0530

    Time: 1 ms (99.41%), Space: 57.1 MB (57.32%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.java
+++ 0053-maximum-subarray/0053-maximum-subarray.java
@@ -1,6 +1,6 @@
 class Solution {
     public int maxSubArray(int[] nums) {
-        int i=0;
+       /* int i=0;
         int sum=0;
         int max=Integer.MIN_VALUE;
 
@@ -12,8 +12,31 @@ class Solution {
                 sum=0;
             }
         }
-        return max;
+        return max;*/
 
+        if(nums.length==1){
+            return nums[0];
+        }
+        int i=0;// start
+        int j=0; //end
+        int maxx=Integer.MIN_VALUE;
+        int sum=0;
+
+        while(i<=j && j<nums.length){
+            sum=sum+nums[j];
+            maxx=Math.max(maxx,sum);
+            
+            while(sum<=0 && i<=j){
+                
+                    sum=sum-nums[i];
+                    i++;
+                  
+            }
+            j++;
+            //maxx=Math.max(maxx,sum);
+            
+        }
+        return maxx;
         
     }
 }
